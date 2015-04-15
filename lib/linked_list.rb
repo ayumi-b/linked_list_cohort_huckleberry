8ee617a50208b56f5166fc4efd47d1819ee26f63
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-04-15 05:40:22 -0500

    5a and 5b seems to be passing for now

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,19 @@
 class LinkedList
+attr_reader :first_item
+
+  def initialize
+    @first_item
+  end
+
+  def push(payload)
+    if @first_item.nil?
+      lli = LinkedListItem.new(payload)
+      @first_item = lli
+    end
+  end
+
+  def get(index)
+    first_item.payload
+  end
 
 end
