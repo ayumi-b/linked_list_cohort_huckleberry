b0c9b97be7e2f3efec791d50c36692a716651410
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-04-15 11:30:17 -0500

    currently passing through 5d

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,19 +1,58 @@
 class LinkedList
-attr_reader :first_item
 
-  def initialize
-    @first_item
-  end
-
-  def push(payload)
-    if @first_item.nil?
-      lli = LinkedListItem.new(payload)
-      @first_item = lli
+  def get(nth_item)
+    if nth_item == 0
+      @first_item.payload
+    else
+      count= 0
+      current_node = @first_item
+      while count < nth_item
+        current_node = current_node.next_item
+        count = count + 1
+      end
+      current_node.payload
     end
   end
 
-  def get(index)
-    first_item.payload
+  def push(item)
+    if @first_item.nil?
+      @first_item = LinkedListItem.new(item)
+      @last_item = @first_item
+    else
+      @last_item.next_item = LinkedListItem.new(item)
+      @last_item = @last_item.next_item
+    end
   end
-
 end
+
+#attr_reader :first_item
+
+  #def initialize
+    #@first_item
+  #end
+
+  #def push(item)
+    #if @first_item.nil?
+      #@first_item = LinkedListItem.new(item)
+      ##@first_item = lli
+    #else
+      #@first_time.next_item = LinkedListItem.new(item)
+    #end
+  #end
+
+  #def get(nth_item)
+    ##first_item.payload
+    #if nth_item == 0
+      #@first_item.payload
+    #else
+      #count = 0
+      #current_node = @first_item
+      #while count < nth_item
+        #current_node = current.node.next_item
+        #count = count + 1
+      #end
+      #current_node.payload
+    #end
+  #end
+
+#end
