6bcf8e3e98a6aba3be1964a994877d505a83d515
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-04-16 12:16:02 -0500

    working upto 9a

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,10 +1,11 @@
 require_relative 'linked_list_item'
 
 class LinkedList
-  attr_reader :size
+  attr_reader :size, :last, :to_s
 
   def initialize
     @size = 0
+    @last
   end
 
   def get(index)
@@ -23,44 +24,27 @@ class LinkedList
 
   def push(payload)
     if @first_item.nil?
-      @first_item = LinkedListItem.new(payload)
-      @last_item = @first_item
+      lli = LinkedListItem.new(payload)
+      @first_item = lli
+      @last = lli
     else
-      @last_item.next_item = LinkedListItem.new(payload)
-      @last_item = @last_item.next_item
+      @last.next_item = LinkedListItem.new(payload)
+      @last = @last.next_item
     end
       @size+= 1
   end
+
+  def last
+    if ! @last.nil?
+      @last.payload.to_s
+    end
+
+  end
+
+  def to_s
+    "| "+ @payload.to_s + "|" 
+  end
 end
 
-#attr_reader :first_item
 
-  #def initialize
-    #@first_item
-  #end
 
-  #def push(item)
-    #if @first_item.nil?
-      #@first_item = LinkedListItem.new(item)
-      ##@first_item = lli
-    #else
-      #@first_time.next_item = LinkedListItem.new(item)
-    #end
-  #end
-
-  #def get(nth_item)
-    ##first_item.payload
-    #if nth_item == 0
-      #@first_item.payload
-    #else
-      #count = 0
-      #current_node = @first_item
-      #while count < nth_item
-        #current_node = current.node.next_item
-        #count = count + 1
-      #end
-      #current_node.payload
-    #end
-  #end
-
-#end
