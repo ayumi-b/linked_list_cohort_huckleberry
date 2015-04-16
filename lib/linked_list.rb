8525b9941733e1affc0b1fc13c881a9ef7223232
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-04-16 10:53:04 -0500

    completed up to 8a

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,28 +7,26 @@ class LinkedList
     @size = 0
   end
 
-  def get(nth_item)
-    raise IndexError if nth_item < 0 || nth_item > @size 
+  def get(index)
+    raise IndexError if index < 0 || index > @size
 
-    if nth_item == 0
+    if index == 0
       @first_item.payload
     else
-      count= 0
       current_node = @first_item
-      while count < nth_item
+      index.times do
         current_node = current_node.next_item
-        count = count + 1
       end
       current_node.payload
     end
   end
 
-  def push(item)
+  def push(payload)
     if @first_item.nil?
-      @first_item = LinkedListItem.new(item)
+      @first_item = LinkedListItem.new(payload)
       @last_item = @first_item
     else
-      @last_item.next_item = LinkedListItem.new(item)
+      @last_item.next_item = LinkedListItem.new(payload)
       @last_item = @last_item.next_item
     end
       @size+= 1
