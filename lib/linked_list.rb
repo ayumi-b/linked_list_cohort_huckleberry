0d31f78c5f7676bba9fb1777fddaa81f0a64245b
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-04-17 15:35:26 -0500

    passing for now through 11

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,11 +1,24 @@
 require_relative 'linked_list_item'
 
 class LinkedList
-  attr_accessor :size, :last_item, :to_s, :ll
+  attr_accessor :size, :first_item, :last_item, :to_s
 
-  def initialize
+  def initialize(*payload)  #*args is always an array
     @size = 0
-    @last_item
+    payload.each { |payload| push(payload)}
+    #if payload[0]
+      #lli = LinkedListItem.new(payload[0])
+      #@first_item = lli
+      #@current_item = lli
+      #while payload[number]
+        #llin = LinkedListItem.new(payload[number])
+        #@last_item.next_item = llin
+        #@last_item = @last_item.next_item
+        #@size += 1
+  end
+
+  def [](index)
+    get(index)
   end
 
   def get(index)
@@ -42,22 +55,35 @@ class LinkedList
     end
 
   end
+  #
+  #def to_s
+    #string = ""
+    #if @size == 0
+      #"| |"
+    #elsif @size == 1
+      #"| #{@first_item.payload} |"
+    #else
+      #(@size - 1).times do |i|
+        #string << "#{get(i)}, "
+      #end
+      #string << "#{@last_item.payload}"
+      #"| #{string} |"
+    #end
+  #end
 
   def to_s
-    string = ""
-    if @size == 0
-      "| |"
-    elsif @size == 1
-      "| #{@first_item.payload} |"
-    else
-      (@size - 1).times do |i|
-        string << "#{get(i)}, "
-      end
-      string << "#{@last_item.payload}"
-      "| #{string} |"
+    str = "| "
+    current_item = @first_item
+    until current_item.nil?
+      str << current_item.payload
+      punctuation = current_item.last? ?  " " : ", "
+      str << punctuation
+      current_item = current_item.next_item
+    end
+    str << "|"
+    str
   end
 
-  end
 end
 
 
